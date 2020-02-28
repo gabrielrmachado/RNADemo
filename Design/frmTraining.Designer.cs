ef7cf328eb195f88b4caa11ff44546c22457f899
Gabriel Machado <gabriel.rmachado10@gmail.com>
2020-02-28 20:17:04 -0300

    First commit.

---
+++ Design/frmTraining.Designer.cs
@@ -0,0 +1,385 @@
+﻿namespace RNADemo
+{
+    partial class frmTraining
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pb20 = new System.Windows.Forms.PictureBox();
+            this.pb19 = new System.Windows.Forms.PictureBox();
+            this.pb18 = new System.Windows.Forms.PictureBox();
+            this.pb17 = new System.Windows.Forms.PictureBox();
+            this.pb16 = new System.Windows.Forms.PictureBox();
+            this.pb15 = new System.Windows.Forms.PictureBox();
+            this.pb14 = new System.Windows.Forms.PictureBox();
+            this.pb13 = new System.Windows.Forms.PictureBox();
+            this.pb12 = new System.Windows.Forms.PictureBox();
+            this.pb11 = new System.Windows.Forms.PictureBox();
+            this.pb10 = new System.Windows.Forms.PictureBox();
+            this.pb9 = new System.Windows.Forms.PictureBox();
+            this.pb4 = new System.Windows.Forms.PictureBox();
+            this.pb3 = new System.Windows.Forms.PictureBox();
+            this.pb2 = new System.Windows.Forms.PictureBox();
+            this.pb1 = new System.Windows.Forms.PictureBox();
+            this.pb8 = new System.Windows.Forms.PictureBox();
+            this.pb7 = new System.Windows.Forms.PictureBox();
+            this.pb6 = new System.Windows.Forms.PictureBox();
+            this.pb5 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pb20)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb19)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb18)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb17)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb16)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb15)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb14)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb13)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb12)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pb20);
+            this.groupBox1.Controls.Add(this.pb19);
+            this.groupBox1.Controls.Add(this.pb18);
+            this.groupBox1.Controls.Add(this.pb17);
+            this.groupBox1.Controls.Add(this.pb16);
+            this.groupBox1.Controls.Add(this.pb15);
+            this.groupBox1.Controls.Add(this.pb14);
+            this.groupBox1.Controls.Add(this.pb13);
+            this.groupBox1.Controls.Add(this.pb12);
+            this.groupBox1.Controls.Add(this.pb11);
+            this.groupBox1.Controls.Add(this.pb10);
+            this.groupBox1.Controls.Add(this.pb9);
+            this.groupBox1.Controls.Add(this.pb4);
+            this.groupBox1.Controls.Add(this.pb3);
+            this.groupBox1.Controls.Add(this.pb2);
+            this.groupBox1.Controls.Add(this.pb1);
+            this.groupBox1.Controls.Add(this.pb8);
+            this.groupBox1.Controls.Add(this.pb7);
+            this.groupBox1.Controls.Add(this.pb6);
+            this.groupBox1.Controls.Add(this.pb5);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(29, 35);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(243, 278);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Padrão para Treinamento";
+            // 
+            // pb20
+            // 
+            this.pb20.BackColor = System.Drawing.Color.White;
+            this.pb20.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pb20.Location = new System.Drawing.Point(166, 223);
+            this.pb20.Name = "pb20";
+            this.pb20.Size = new System.Drawing.Size(30, 30);
+            this.pb20.TabIndex = 39;
+            this.pb20.TabStop = false;
+            this.pb20.Click += new System.EventHandler(this.pb20_Click);
+            // 
+            // pb19
+            // 
+            this.pb19.BackColor = System.Drawing.Color.White;
+            this.pb19.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pb19.Location = new System.Drawing.Point(137, 223);
+            this.pb19.Name = "pb19";
+            this.pb19.Size = new System.Drawing.Size(30, 30);
+            this.pb19.TabIndex = 38;
+            this.pb19.TabStop = false;
+            this.pb19.Click += new System.EventHandler(this.pb19_Click);
+            // 
+            // pb18
+            // 
+            this.pb18.BackColor = System.Drawing.Color.White;
+            this.pb18.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pb18.Location = new System.Drawing.Point(108, 223);
+            this.pb18.Name = "pb18";
+            this.pb18.Size = new System.Drawing.Size(30, 30);
+            this.pb18.TabIndex = 37;
+            this.pb18.TabStop = false;
+            this.pb18.Click += new System.EventHandler(this.pb18_Click);
+            // 
+            // pb17
+            // 
+            this.pb17.BackColor = System.Drawing.Color.White;
+            this.pb17.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pb17.Location = new System.Drawing.Point(79, 223);
+            this.pb17.Name = "pb17";
+            this.pb17.Size = new System.Drawing.Size(30, 30);
+            this.pb17.TabIndex = 36;
+            this.pb17.TabStop = false;
+            this.pb17.Click += new System.EventHandler(this.pb17_Click);
+            // 
+            // pb16
+            // 
+            this.pb16.BackColor = System.Drawing.Color.White;
+            this.pb16.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pb16.Location = new System.Drawing.Point(166, 193);
+            this.pb16.Name = "pb16";
+            this.pb16.Size = new System.Drawing.Size(30, 30);
+            this.pb16.TabIndex = 35;
+            this.pb16.TabStop = false;
+            this.pb16.Click += new System.EventHandler(this.pb16_Click);
+            // 
+            // pb15
+            // 
+            this.pb15.BackColor = System.Drawing.Color.White;
+            this.pb15.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pb15.Location = new System.Drawing.Point(137, 193);
+            this.pb15.Name = "pb15";
+            this.pb15.Size = new System.Drawing.Size(30, 30);
+            this.pb15.TabIndex = 34;
+            this.pb15.TabStop = false;
+            this.pb15.Click += new System.EventHandler(this.pb15_Click);
+            // 
+            // pb14
+            // 
+            this.pb14.BackColor = System.Drawing.Color.White;
+            this.pb14.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pb14.Location = new System.Drawing.Point(108, 193);
+            this.pb14.Name = "pb14";
+            this.pb14.Size = new System.Drawing.Size(30, 30);
+            this.pb14.TabIndex = 33;
+            this.pb14.TabStop = false;
+            this.pb14.Click += new System.EventHandler(this.pb14_Click);
+            // 
+            // pb13
+            // 
+            this.pb13.BackColor = System.Drawing.Color.White;
+            this.pb13.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pb13.Location = new System.Drawing.Point(79, 193);
+            this.pb13.Name = "pb13";
+            this.pb13.Size = new System.Drawing.Size(30, 30);
+            this.pb13.TabIndex = 32;
+            this.pb13.TabStop = false;
+            this.pb13.Click += new System.EventHandler(this.pb13_Click);
+            // 
+            // pb12
+            // 
+            this.pb12.BackColor = System.Drawing.Color.White;
+            this.pb12.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pb12.Location = new System.Drawing.Point(166, 163);
+            this.pb12.Name = "pb12";
+            this.pb12.Size = new System.Drawing.Size(30, 30);
+            this.pb12.TabIndex = 31;
+            this.pb12.TabStop = false;
+            this.pb12.Click += new System.EventHandler(this.pb12_Click);
+            // 
+            // pb11
+            // 
+            this.pb11.BackColor = System.Drawing.Color.White;
+            this.pb11.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pb11.Location = new System.Drawing.Point(137, 163);
+            this.pb11.Name = "pb11";
+            this.pb11.Size = new System.Drawing.Size(30, 30);
+            this.pb11.TabIndex = 30;
+            this.pb11.TabStop = false;
+            this.pb11.Click += new System.EventHandler(this.pb11_Click);
+            // 
+            // pb10
+            // 
+            this.pb10.BackColor = System.Drawing.Color.White;
+            this.pb10.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pb10.Location = new System.Drawing.Point(108, 163);
+            this.pb10.Name = "pb10";
+            this.pb10.Size = new System.Drawing.Size(30, 30);
+            this.pb10.TabIndex = 29;
+            this.pb10.TabStop = false;
+            this.pb10.Click += new System.EventHandler(this.pb10_Click);
+            // 
+            // pb9
+            // 
+            this.pb9.BackColor = System.Drawing.Color.White;
+            this.pb9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pb9.Location = new System.Drawing.Point(79, 163);
+            this.pb9.Name = "pb9";
+            this.pb9.Size = new System.Drawing.Size(30, 30);
+            this.pb9.TabIndex = 28;
+            this.pb9.TabStop = false;
+            this.pb9.Click += new System.EventHandler(this.pb9_Click);
+            // 
+            // pb4
+            // 
+            this.pb4.BackColor = System.Drawing.Color.White;
+            this.pb4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pb4.Location = new System.Drawing.Point(166, 103);
+            this.pb4.Name = "pb4";
+            this.pb4.Size = new System.Drawing.Size(30, 30);
+            this.pb4.TabIndex = 27;
+            this.pb4.TabStop = false;
+            this.pb4.Click += new System.EventHandler(this.pb4_Click);
+            // 
+            // pb3
+            // 
+            this.pb3.BackColor = System.Drawing.Color.White;
+            this.pb3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pb3.Location = new System.Drawing.Point(137, 103);
+            this.pb3.Name = "pb3";
+            this.pb3.Size = new System.Drawing.Size(30, 30);
+            this.pb3.TabIndex = 26;
+            this.pb3.TabStop = false;
+            this.pb3.Click += new System.EventHandler(this.pb3_Click);
+            // 
+            // pb2
+            // 
+            this.pb2.BackColor = System.Drawing.Color.White;
+            this.pb2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pb2.Location = new System.Drawing.Point(108, 103);
+            this.pb2.Name = "pb2";
+            this.pb2.Size = new System.Drawing.Size(30, 30);
+            this.pb2.TabIndex = 25;
+            this.pb2.TabStop = false;
+            this.pb2.Click += new System.EventHandler(this.pb2_Click);
+            // 
+            // pb1
+            // 
+            this.pb1.BackColor = System.Drawing.Color.White;
+            this.pb1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pb1.Location = new System.Drawing.Point(79, 103);
+            this.pb1.Name = "pb1";
+            this.pb1.Size = new System.Drawing.Size(30, 30);
+            this.pb1.TabIndex = 24;
+            this.pb1.TabStop = false;
+            this.pb1.Click += new System.EventHandler(this.pb1_Click);
+            // 
+            // pb8
+            // 
+            this.pb8.BackColor = System.Drawing.Color.White;
+            this.pb8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pb8.Location = new System.Drawing.Point(166, 133);
+            this.pb8.Name = "pb8";
+            this.pb8.Size = new System.Drawing.Size(30, 30);
+            this.pb8.TabIndex = 23;
+            this.pb8.TabStop = false;
+            this.pb8.Click += new System.EventHandler(this.pb8_Click);
+            // 
+            // pb7
+            // 
+            this.pb7.BackColor = System.Drawing.Color.White;
+            this.pb7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pb7.Location = new System.Drawing.Point(137, 133);
+            this.pb7.Name = "pb7";
+            this.pb7.Size = new System.Drawing.Size(30, 30);
+            this.pb7.TabIndex = 22;
+            this.pb7.TabStop = false;
+            this.pb7.Click += new System.EventHandler(this.pb7_Click);
+            // 
+            // pb6
+            // 
+            this.pb6.BackColor = System.Drawing.Color.White;
+            this.pb6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pb6.Location = new System.Drawing.Point(108, 133);
+            this.pb6.Name = "pb6";
+            this.pb6.Size = new System.Drawing.Size(30, 30);
+            this.pb6.TabIndex = 21;
+            this.pb6.TabStop = false;
+            this.pb6.Click += new System.EventHandler(this.pb6_Click);
+            // 
+            // pb5
+            // 
+            this.pb5.BackColor = System.Drawing.Color.White;
+            this.pb5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pb5.Location = new System.Drawing.Point(79, 133);
+            this.pb5.Name = "pb5";
+            this.pb5.Size = new System.Drawing.Size(30, 30);
+            this.pb5.TabIndex = 20;
+            this.pb5.TabStop = false;
+            this.pb5.Click += new System.EventHandler(this.pb5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Configuração e Treinamento da Rede Neural";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pb20)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb19)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb18)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb17)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb16)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb15)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb14)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb13)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb12)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb5)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pb20;
+        private System.Windows.Forms.PictureBox pb19;
+        private System.Windows.Forms.PictureBox pb18;
+        private System.Windows.Forms.PictureBox pb17;
+        private System.Windows.Forms.PictureBox pb16;
+        private System.Windows.Forms.PictureBox pb15;
+        private System.Windows.Forms.PictureBox pb14;
+        private System.Windows.Forms.PictureBox pb13;
+        private System.Windows.Forms.PictureBox pb12;
+        private System.Windows.Forms.PictureBox pb11;
+        private System.Windows.Forms.PictureBox pb10;
+        private System.Windows.Forms.PictureBox pb9;
+        private System.Windows.Forms.PictureBox pb4;
+        private System.Windows.Forms.PictureBox pb3;
+        private System.Windows.Forms.PictureBox pb2;
+        private System.Windows.Forms.PictureBox pb1;
+        private System.Windows.Forms.PictureBox pb8;
+        private System.Windows.Forms.PictureBox pb7;
+        private System.Windows.Forms.PictureBox pb6;
+        private System.Windows.Forms.PictureBox pb5;
+    }
+}
+
---
+++ Design/frmTraining.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RNADemo
+{
+    public partial class frmTraining : Form
+    {
+        public frmTraining()
+        {
+            InitializeComponent();
+        }
+
+        private void ChangeColor(object sender)
+        {
+            var pb = (PictureBox)sender;
+
+            if (pb.BackColor == Color.Black)
+                pb.BackColor = Color.White;
+            else
+                pb.BackColor = Color.Black;
+        }
+
+        private void pb11_Click(object sender, EventArgs e)
+        {
+            ChangeColor(sender);
+        }
+
+        private void pb19_Click(object sender, EventArgs e)
+        {
+            ChangeColor(sender);
+        }
+
+        private void pb18_Click(object sender, EventArgs e)
+        {
+            ChangeColor(sender);
+        }
+
+        private void pb17_Click(object sender, EventArgs e)
+        {
+            ChangeColor(sender);
+        }
+
+        private void pb16_Click(object sender, EventArgs e)
+        {
+            ChangeColor(sender);
+        }
+
+        private void pb15_Click(object sender, EventArgs e)
+        {
+            ChangeColor(sender);
+        }
+
+        private void pb14_Click(object sender, EventArgs e)
+        {
+            ChangeColor(sender);
+        }
+
+        private void pb13_Click(object sender, EventArgs e)
+        {
+            ChangeColor(sender);
+        }
+
+        private void pb12_Click(object sender, EventArgs e)
+        {
+            ChangeColor(sender);
+        }
+
+        private void pb20_Click(object sender, EventArgs e)
+        {
+            ChangeColor(sender);
+        }
+
+        private void pb10_Click(object sender, EventArgs e)
+        {
+            ChangeColor(sender);
+        }
+
+        private void pb9_Click(object sender, EventArgs e)
+        {
+            ChangeColor(sender);
+        }
+
+        private void pb4_Click(object sender, EventArgs e)
+        {
+            ChangeColor(sender);
+        }
+
+        private void pb3_Click(object sender, EventArgs e)
+        {
+            ChangeColor(sender);
+        }
+
+        private void pb2_Click(object sender, EventArgs e)
+        {
+            ChangeColor(sender);
+        }
+
+        private void pb1_Click(object sender, EventArgs e)
+        {
+            ChangeColor(sender);
+        }
+
+        private void pb8_Click(object sender, EventArgs e)
+        {
+            ChangeColor(sender);
+        }
+
+        private void pb7_Click(object sender, EventArgs e)
+        {
+            ChangeColor(sender);
+        }
+
+        private void pb6_Click(object sender, EventArgs e)
+        {
+            ChangeColor(sender);
+        }
+
+        private void pb5_Click(object sender, EventArgs e)
+        {
+            ChangeColor(sender);
+        }
+    }
+}
